a8c646d0887b9f9a3e4e7c64d0857b17ca713dc7
RVABHINAV123 <revellivenkataabhinav@gmail.com>
2026-08-13 11:43:46 +0530

    Fix CORS for frontend

--- backend/main.py
+++ backend/main.py
@@ -15,12 +15,14 @@ app = FastAPI(
 # Since these are different origins, CORS permission is required.
 app.add_middleware(
     CORSMiddleware,
-  allow_origins=[
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:5174",
-],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:5175",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:5174",
+        "http://127.0.0.1:5175",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
